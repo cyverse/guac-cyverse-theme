e1aa514ecaab2b2ad7408cf8caa383272e27557d
Calvin Mclean <calvinmclean@email.arizona.edu>
2018-02-12 10:56:24 -0700

    Use hide-able div at bottom of screen for help message

--- js/help.js
+++ js/help.js
@@ -1,2 +1,5 @@
 // Provide a help message for the user
 // window.alert("HELP MESSAGE\nMore info: https://guacamole.apache.org/")
+function hideHelp() {
+  document.getElementById("help_section").style["visibility"] = "hidden";
+}
