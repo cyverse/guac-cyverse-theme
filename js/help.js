11b8a8dc2251cd035780428514b3b39b0d941fe8
Calvin Mclean <calvinmclean@email.arizona.edu>
2018-06-29 13:33:51 -0700

    Add download/upload hint and make div hide-able

--- js/help.js
+++
@@ -1,5 +0,0 @@
-// Provide a help message for the user
-// window.alert("HELP MESSAGE\nMore info: https://guacamole.apache.org/")
-// function hideHelp() {
-//   document.getElementById("help-section").style["visibility"] = "hidden";
-// }
